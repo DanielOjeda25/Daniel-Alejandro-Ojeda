bc051219b23ee134215822350e05d9aa3003c22c
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-19 18:50:05 -0300

    se agrego un navbar, utilizando react-icons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,51 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { FaBars, FaTimes, FaCode, FaUserNinja, FaPhone } from 'react-icons/fa'
+import { GiPortal } from "react-icons/gi"
 import './styles.css'
-import logo from '../../assets/portal-9.svg'
-function Navbar() {
 
-  const toggleMobileMenu = ({menu}) => {
-    menu.classList.toggle('open')
+function Navbar() {
+  const navRef = useRef()
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle('responsive_nav')
   }
+
   return (
-    <header className='header'>
-      {/* Arreglar el logo y el texto, dejar en una linea */}    
-      <div className='logo'>
-        <a>
-          <img src={logo} className='logo-portal'/>
-          <h4 className='text-logo'>Hello World!</h4>
-        </a>
-      </div>
-      <nav className='nav'>
-        <ul>
-          <li>
-            <a href='#projects'>Projects</a>
-          </li>
-          <li>
-            <a href='#about'>About</a>
-          </li>
-          <li>
-            <a href='#contact'>Contact</a>
-          </li>
-        </ul>
+    <header>
+      <a href='/' className='logo' >
+      <GiPortal className='Fa-portal' />
+      <h5 className='text-logo'>Hello World!</h5>
+      </a>
+      <nav ref={navRef}>
+        <a href='/'><FaCode className='Fa-icon' />Projects</a>
+        <a href='#h'><FaUserNinja className='Fa-about' />About Me</a>
+        <a href='#h'><FaPhone className='Fa-phone' />Contact</a>
+        <button className='nav-btn nav-close-btn' onClick={showNavBar}>
+          <FaTimes />
+        </button>
       </nav>
-      <div id='hamburger-icon' onClick={toggleMobileMenu}>
-        <div className='bar1'></div>
-        <div className='bar2'></div>
-        <div className='bar3'></div>
-        <ul className='mobile-toggle'>
-          <ul>
-            <li>
-              <a href='#projects'>Projects</a>
-            </li>
-            <li>
-              <a href='#about'>About</a>
-            </li>
-            <li>
-              <a href='#contact'>Contact</a>
-            </li>
-          </ul>
-        </ul>
-      </div>
+      <button className='nav-btn' onClick={showNavBar}>
+        <FaBars />
+      </button>
     </header>
   )
 }
