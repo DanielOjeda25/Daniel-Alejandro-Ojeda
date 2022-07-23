12d280ba7054d4d172230bfef77c846c5e4db07a
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-23 13:55:16 -0300

    Nuevo component que se muestra

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
 import './App.css'
+import Animation from './components/animation/index'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
-   <Navbar />
+    <>
+      <Navbar />
+      <Animation />
+    </>
+
   )
 }
 
