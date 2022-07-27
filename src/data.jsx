ec8c32edbe2ebcb916ae977cd7a6b7e917a93ae4
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-27 19:16:50 -0300

    cambios minimos

---
+++ src/data.jsx
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    id: 1,
+    text: 'About Me',
+    url: '#about',
+  }
+  ,
+  {
+    id: 2,
+    text: 'Projects',
+    url: '#projects',
+  },
+  {
+    id: 3,
+    text: 'Contact',
+    url:'#contact',
+  }
+]
