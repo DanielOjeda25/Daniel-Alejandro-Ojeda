2f4b5d0f5ebde5b8eafe461d585b74a228f9fa56
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-30 11:02:39 -0300

    navbar , cambios minimos

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,23 +11,23 @@ function Navbar() {
   }
 
   return (
-    <header>
-      <a href='#home' className='logo' >
-        <GiPortal className='Fa-portal' />
-        <h5 className='text-logo'>Hello World!</h5>
-      </a>
-      <nav ref={navRef}>
-        <a href='#about'><FaUserNinja className='Fa-about' />About Me</a>
-        <a href='/'><FaCode className='Fa-icon' />Projects</a>
-        <a href='/'><FaPhone className='Fa-phone' />Contact</a>
-        <button className='nav-btn nav-close-btn' onClick={showNavBar}>
-          <FaTimes />
+      <header>
+        <a href='#home' className='logo' >
+          <GiPortal className='Fa-portal' />
+          <h5 className='text-logo'>Hello World!</h5>
+        </a>
+        <nav ref={navRef}>
+          <a href='#about'><FaUserNinja className='Fa-about' />About Me</a>
+          <a href='#projects'><FaCode className='Fa-icon' />Projects</a>
+          <a href='/'><FaPhone className='Fa-phone' />Contact</a>
+          <button className='nav-btn nav-close-btn' onClick={showNavBar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className='nav-btn' onClick={showNavBar}>
+          <FaBars />
         </button>
-      </nav>
-      <button className='nav-btn' onClick={showNavBar}>
-        <FaBars />
-      </button>
-    </header>
+      </header>
   )
 }
 
