4f34909b08132a99a45cba112a8b26d37e484df5
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-30 10:59:58 -0300

    Nuevo component/Page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Animation from '../components/animation'
+import Navbar from '../components/Navbar'
+import Proyects from '../components/Proyects'
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Animation />
+    </>
+  )
+}
+
+export default Home
