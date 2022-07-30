42390a630f553f5a4a4cbdb508cafd8b8372b02e
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-30 11:01:41 -0300

    Card component

---
+++ src/components/Proyects/Card.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './styles.css'
+
+function Card(props) {
+
+  return (
+    <section>
+        <div className='card-box'>
+          <div className='card-image'>
+            <img src={props.img} alt='logo' />
+          </div>
+          <div className='card-title'>
+            {props.title}
+          </div>
+          <div className='card-text'>
+            {props.text}
+          </div>
+        </div>
+    </section>
+
+  )
+}
+
+export default Card
