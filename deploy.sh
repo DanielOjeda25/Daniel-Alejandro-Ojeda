405357ee45c2c907fa2c2a4d912c9c30b9f7ce71
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-06 19:22:36 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:DanielOjeda25/MyPortfolio.git main:gh-pages
+# git push -f git@github.com:DanielOjeda25/Daniel-Alejandro-Ojeda.git main:gh-pages
 
