2de06b5c05662078d6ed71a1ed7958aa000decdb
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-07 11:36:06 -0300

    minimal changes

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ function Navbar() {
 
   return (
     <header>
-      <a href='/' className='logo' >
+      <a href='https://danielojeda25.github.io/Daniel-Alejandro-Ojeda/' className='logo' >
         <GiPortal className='Fa-portal' />
         <h5 className='text-logo'>Hello World!</h5>
       </a>
