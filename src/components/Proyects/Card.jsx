33fec07b7efeb28052b74b630347b3a30f030158
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-17 14:32:53 -0300

    props added

--- src/components/Proyects/Card.jsx
+++ src/components/Proyects/Card.jsx
@@ -9,15 +9,15 @@ function Card(props) {
         <div className='card-image'>
           <img src={props.img} alt='logo' />
         </div>
-        <div className='card-title'>
-          {props.title}
-        </div>
-        <div className='card-text'>
-          {props.text}
+        <div className='card-title'>{props.title}</div>
+        <div className='card-text'>{props.text}</div>
+        <div className='card-button'>
+          <a href= {props.links} target='_blank'>
+            Website
+          </a>
         </div>
       </div>
     </section>
-
   )
 }
 
